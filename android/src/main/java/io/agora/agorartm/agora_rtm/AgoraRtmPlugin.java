57409262d630d11d8ba92bb77432d299f6d32b47
Mohamed Radwan <radwan@zetaton.com>
2025-05-21 23:37:03 +0300

    update

--- android/src/main/java/io/agora/agorartm/agora_rtm/AgoraRtmPlugin.java
+++ android/src/main/java/io/agora/agorartm/agora_rtm/AgoraRtmPlugin.java
@@ -1,15 +1,12 @@
-package io.agora.agorartm.agora_rtm;
+package io.agora.agorartm;
 
 import androidx.annotation.NonNull;
-
-import io.flutter.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/** AgoraRtmPlugin */
 public class AgoraRtmPlugin implements FlutterPlugin, MethodCallHandler {
   private MethodChannel channel;
 
@@ -21,15 +18,8 @@ public class AgoraRtmPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if ("androidInit".equals(call.method)) {
-      // dart ffi DynamicLibrary.open do not trigger JNI_OnLoad in iris, so we need call java
-      // System.loadLibrary here to trigger the JNI_OnLoad explicitly.
-      try {
-        System.loadLibrary("AgoraRtmWrapper");
-      } catch (Exception e) {
-        Log.e("AgoraRtmPlugin", "androidInit error:\n" + e);
-      }
-      result.success(0);
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else {
       result.notImplemented();
     }
